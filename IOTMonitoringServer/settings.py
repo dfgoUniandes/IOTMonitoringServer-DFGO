a8bc921ff30fb20eee0169e1025bb7075f3ebb6d
Diego Felipe Garcia Ortega <df.garciao1@uniandes.edu.co>
2022-09-21 15:27:13 -0500

    changes urls for challenge 7

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.95.243.233"]
+ALLOWED_HOSTS = ["localhost", "54.174.30.148"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.172.34.1",  # Dirección IP de la base de datos
+        "HOST": "52.91.185.16",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.209.176.142"
+MQTT_HOST = "54.196.25.75"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
